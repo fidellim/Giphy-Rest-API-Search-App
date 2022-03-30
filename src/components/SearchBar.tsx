096e7d6b7a7525f75a69ce7282edf672fd196809
fidellim <lim.fidel@gmail.com>
2022-03-30 19:06:57 +0400

    add search bar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+
+const SearchBar = () => {
+  return (
+    <>
+        <Container>
+            <div>SearchBar</div>
+        </Container>
+    </>
+  )
+}
+
+export default SearchBar
