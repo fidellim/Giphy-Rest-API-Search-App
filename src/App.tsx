e8816e49cada0a7792ab9fe148ff9ed53983e079
fidellim <lim.fidel@gmail.com>
2022-03-30 19:09:16 +0400

    add searchbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavbar from './components/MyNavbar';
-
+import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (
     <div>
       <MyNavbar />
+      <SearchBar />
     </div>
   )
 }
