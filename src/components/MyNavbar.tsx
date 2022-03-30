6794c53fd571e6009249ccd1e0f272a66110c2ba
fidellim <lim.fidel@gmail.com>
2022-03-30 18:24:51 +0400

    add toggle switch and implement dark/light theme

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -8,15 +8,20 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 const MyNavbar = () => {
+  const html = document.querySelector('html');
   const [isToggled, setIsToggled] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState("theme-light");
+  if (html)
+    html.dataset.theme = isDarkTheme;
 
   const handleToggle = () => {
       setIsToggled(!isToggled);
+      (isDarkTheme === "theme-dark") ?setIsDarkTheme("theme-light") : setIsDarkTheme("theme-dark")
   }
 
   return (
     <>
-      <Navbar bg="dark" variant="dark" className="py-3">
+      <Navbar className="py-3 shadow navbar">
         <Container>
           <Navbar.Brand href="#home">
             <img
@@ -26,7 +31,7 @@ const MyNavbar = () => {
               height="30"
               className="d-inline-block align-top"
             />
-            <h4 className="ms-3" style={{display:"inline"}}>
+            <h4 className="ms-3 d-inline navbarTitle">
               Giphy REST API
             </h4>
           </Navbar.Brand>
@@ -34,15 +39,16 @@ const MyNavbar = () => {
             onChange={handleToggle}
             checked={isToggled}
             width={70}
-            offColor="#7f5539"
-            onColor="#FCDAB7"
-            offHandleColor="#FCDAB7"
-            onHandleColor="#7f5539"
+            offColor="#165c9e"
+            onColor="#a2d2ff"
+            offHandleColor="#003566"
+            onHandleColor="#45a5ff"
             handleDiameter={35}
             boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
             activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
             uncheckedIcon={<DarkModeIcon className="fs-5" style={{marginLeft: 12, color: "white"}}/>}
-            checkedIcon={<LightModeIcon className="fs-5 ms-2" />} />
+            checkedIcon={<LightModeIcon className="fs-5 ms-2" />}
+          />
         </Container>
       </Navbar>
     </>
