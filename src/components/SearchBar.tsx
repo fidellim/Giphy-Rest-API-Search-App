376fc04a4277be82cca3861555e025b1b90bb5db
fidellim <lim.fidel@gmail.com>
2022-03-31 12:28:38 +0400

    add searchbar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
-  return (
-    <>
-        <Container>
-            <div>SearchBar</div>
-        </Container>
-    </>
-  )
-}
+	const handleSearch = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+	};
 
-export default SearchBar
+	return (
+		<>
+			<main className="pt-5 pb-3">
+				<Container>
+					<form className="d-flex py-3 align-items-center justify-content-center">
+						<div className="position-relative w-50 me-2">
+							<SearchIcon className="search-icon fs-3 position-absolute top-50 start-0 translate-middle ms-5" />
+							<input
+								// data-testid="inputUser"
+								type="text"
+								className="search-bar w-100 py-2 ps-6 pe-2 rounded"
+								placeholder="Search a GIF"
+								// value=""
+								// onChange={}
+							/>
+						</div>
+
+						<button
+							className="search-button button border-0 py-2 px-5 rounded"
+							onClick={handleSearch}
+						>
+							Search
+						</button>
+					</form>
+				</Container>
+			</main>
+		</>
+	);
+};
+
+export default SearchBar;
