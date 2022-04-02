a9b688775c2f361b470306fbf12f2fd545210ac2
fidellim <lim.fidel@gmail.com>
2022-04-02 11:25:28 +0400

    convert vanilla css to bootstrap

--- src/components/NoResultFound.tsx
+++ src/components/NoResultFound.tsx
@@ -4,10 +4,10 @@ import no_result_img from "../assets/no_result_found.png";
 
 const NoResultFound = () => {
 	return (
-		<Container className="no-result-container">
-			<div className="no-result">
+		<Container className="d-flex flex-column justify-content-center align-items-center">
+			<div className="no-result d-flex flex-column align-items-center my-4 py-5 px-5 text-center">
 				<img
-					className="no-result-img .d-block"
+					className="no-result-img .d-block h-auto mb-4"
 					src={no_result_img}
 					alt="no result"
 				/>
