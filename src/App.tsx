e5d7bef69b43e5e21bc6904d2eafbb80ceeb9373
fidellim <lim.fidel@gmail.com>
2022-04-03 23:20:32 +0400

    restructure app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/MyNavbar";
 import SearchBar from "./components/SearchBar";
 import ButtonScroll from "./components/ButtonScroll";
+import GifLayout from "./layout/GifLayout";
 
 const App = () => {
 	return (
 		<>
 			<MyNavbar />
-			<SearchBar />
+			<main className="pt-5 pb-3">
+				<SearchBar />
+				<GifLayout />
+			</main>
 			<ButtonScroll />
 		</>
 	);
