717d6753e81ef67cf915dedecee27d5121e09ce5
fidellim <lim.fidel@gmail.com>
2022-04-03 23:21:55 +0400

    remove react import

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./scss/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
